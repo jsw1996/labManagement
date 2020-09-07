639c97cc474d1940e27763449e55bf5f94455063
Shengwen Jin <jsw1996@uw.edu>
2020-09-07 08:34:50 -0700

    json uploader v2

--- src/app/json_to_firestore/uploader.js
+++ src/app/json_to_firestore/uploader.js
@@ -8,31 +8,22 @@ admin.initializeApp({
 });
 
 const firestore = admin.firestore();
-const path = require("path");
-const fs = require("fs");
-const directoryPath = path.join(__dirname, "files");
+// const path = require("path");
+// const fs = require("fs");
+// const directoryPath = path.join(__dirname, "files");
 
-fs.readdir(directoryPath, function (err, files) {
-  if (err) {
-    return console.log("Unable to scan directory: " + err);
-  }
+var file = "slackProfile.json"
+var menu = require("./files/" + file);
 
-  files.forEach(function (file) {
-    var lastDotIndex = file.lastIndexOf(".");
-
-    var menu = require("./files/" + file);
-
-    menu.forEach(function (obj) {
-      firestore
-        .collection(file.substring(0, lastDotIndex))
-        .doc()
-        .set(obj)
-        .then(function (docRef) {
-          console.log("Document written");
-        })
-        .catch(function (error) {
-          console.error("Error adding document: ", error);
-        });
+menu.forEach(function (obj) {
+  firestore
+    .collection("test3")
+    .doc()
+    .set(obj)
+    .then(function (docRef) {
+      console.log("Document written");
+    })
+    .catch(function (error) {
+      console.error("Error adding document: ", error);
     });
-  });
 });
